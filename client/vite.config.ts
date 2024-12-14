1de52b4863217fe5515800cdff3bc7b1ea958dbf
resized <shai.yarden@gmail.com>
2024-12-14 12:34:54 +0200

    dockerized dev and prod environments

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,7 +5,9 @@ import react from "@vitejs/plugin-react"
 export default defineConfig({
   plugins: [react()],
   server: {
-    open: true,
+    host: "0.0.0.0", // Listen on all interfaces
+    port: 5173,
+    strictPort: true, // Exit if the port is already in use
   },
   test: {
     globals: true,
